061dbecbaed33d9c24789fd0e830daa13b5b6071
skudelko73-ops <skudelko73@gmail.com>
2026-04-14 02:52:20 +1000

    Update server.js

--- server.js
+++ server.js
@@ -599,4 +599,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server on port $
+server.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server on port ${PORT}`));
